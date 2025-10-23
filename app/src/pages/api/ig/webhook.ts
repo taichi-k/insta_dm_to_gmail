cf7058c18ec6706830149ad51ae76192ff55b563
taichi-k <no3b1219@gmail.com>
2025-10-24 00:44:25 +0900

    リージョンを修正

--- app/src/pages/api/ig/webhook.ts
+++ app/src/pages/api/ig/webhook.ts
@@ -5,7 +5,7 @@ import getRawBody from 'raw-body'
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 const ses = new SESClient({
-    region: process.env.AWS_REGION ?? "us-east-1",
+    region: process.env.AWS_REGION ?? "ap-northeast-1",
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID!,       // Vercel環境変数に設定
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,// Vercel環境変数に設定
